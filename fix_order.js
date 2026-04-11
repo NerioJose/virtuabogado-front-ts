9ded900d8184b02fa31616a79b19919ceeda721f
NerioJose <neriojose531@gmail.com>
2026-04-11 13:56:27 -0500

    Se elimina archivos basura de la raiz del proyecto

--- fix_order.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-    const o = await prisma.order.findFirst({ orderBy:{createdAt:'desc'}});
-    console.log("LAST ORDER:", o);
-}
-main().catch(console.error).finally(()=>prisma.$disconnect());
